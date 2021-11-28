323858bba3d8032b45542b5a5ec1a6c11d8d37f9
Rafa Rodríguez <rafa.rodricalve@alum.uca.es>
2021-11-28 11:00:07 +0100

    resized 2 images, starting to develop Tests.jsx component and some css changes done

--- src/components/Tests.jsx
+++ src/components/Tests.jsx
@@ -3,9 +3,505 @@ import React from "react";
 export const Tests = () => {
 
   return (
-        <div>
+    <div>
 
-Tests
+      <form 
+        className = "tests-form"
+        action="#"
+        method="POST"
+        target="_blank"
+      >
+        <legend name="contact">
+          Complete su cuestionario
+        </legend>
+
+        <br />
+
+        <div className="form-group">
+            <b>1. &nbsp;</b> ¿El afectado se mira constantemente en el espejo resaltando sus defectos ante todo?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
         </div>
-    );
+
+        <br />
+         
+        <br />
+
+
+        <div className="form-group">
+        <b>2. &nbsp;</b>¿Siempre que ve alguien con buen físico tiende a comparar su cuerpo con el de la otra persona
+y siempre se ve inferior a esa persona?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>3. &nbsp;</b>¿El afectado siente estrés si falta un día a entrenar?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>4. &nbsp;</b>¿Si un día no cumple con su dieta y sus macronutrientes siente estrés o ansiedad?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>5. &nbsp;</b>¿Le da mas prioridad al gimnasio o hacer ejercicio que a las demás cosas en su vida?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>6. &nbsp;</b>¿Siente que su físico no es suficiente y se ve delgado sin estarlo?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>7. &nbsp;</b>¿El afectado se ha llegado a replantear la toma de esteroides por que se ve "delgado"? 
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>8. &nbsp;</b>¿El afectado ha tenido una bajada de peso excesiva o no logra el aumento de peso previsto?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>9. &nbsp;</b>¿El afectado tiene un aspecto delgado y aun así no se ve delgado?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>10. &nbsp;</b>¿El afectado sufre de fatiga, mareos o incluso desmayos? 
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>11. &nbsp;</b>¿El afectado controla su ingesta de alimentos y suele hacer ayuno?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>12. &nbsp;</b>¿El afectado hace ejercicios cardiovasculares de forma excesiva?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>13. &nbsp;</b>¿El afectado suele mentir sobre la cantidad de ingesta de alimentos?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>14. &nbsp;</b>¿El afectado siente estrés al comer en público?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>15. &nbsp;</b>¿El afectado suele sentirse mal o culpable después de una ingesta no excesiva de alimentos? 
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>16. &nbsp;</b>¿El afectado se fuerza el vómito después de comer para no engordar? 
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>17. &nbsp;</b>¿El afectado suele hacer demasiado ejercicio cardiovascular después de un “atracón” (ingesta
+excesiva de alimentos)? 
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>18. &nbsp;</b>¿El afectado tiene episodios de comer cantidades excesivas de comida de una sola vez?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>19. &nbsp;</b>¿El afectado hace ayunos entre “atracones” (ingestas excesivas de alimentos)?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <div className="form-group">
+        <b>20. &nbsp;</b>¿El afectado usa laxantes, diuréticos o enemas sin ser necesarios?
+            <br /> <br />
+            <label className ="label-si" for="si">Sí  &nbsp; &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+          <br />
+            <label className ="label-no" for="si">No  &nbsp; </label>
+            <input
+            type="radio"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+
+        <br />
+        <br />
+
+
+        <button type="submit" className="btn btn-dark btn-form">Enviar</button>
+      </form>
+    </div>
+  );
 };
