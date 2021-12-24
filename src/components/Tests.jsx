75990ef6d58c859f69823d0f8bbee0cb62ec2974
Rafa Rodríguez <rafa.rodricalve@alum.uca.es>
2021-12-24 14:45:04 +0100

    <br> tag added

--- src/components/Tests.jsx
+++ src/components/Tests.jsx
@@ -65,6 +65,7 @@ export const Tests = () => {
           Vigorexia: ${vigorexia}
           <br>
           Anorexia: ${anorexia}
+          <br>
           Bulimia: ${bulimia}
           <br>
         `;
